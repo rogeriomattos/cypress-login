62c7a151b2fa297930586116f4c4a27af4a36fab
Rogerio Almeida de Mattos <rogeriomattos38@outlook.com>
2023-09-17 12:04:13 -0300

    Criando a tela de login

--- app/src/pages/Login/index.tsx
+++ app/src/pages/Login/index.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/Auth";
+import "./styles.css";
 
 const Login = () => {
   const { user, login } = useAuth();
@@ -7,10 +8,21 @@ const Login = () => {
     // user is not authenticated
     return <Navigate to="/" />;
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login({
+      name: "Loid Ford",
+    });
+  };
+
   return (
-    <div>
-      LOGIN<button onClick={() => login("xx")}>test</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Bem-vindo ao gerenciador de usuários</h1>
+      <input type="text" name="username" placeholder="User Name" />
+      <input type="password" name="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
   );
 };
 
