a520d314d1391cf8133d422ca272a68f1f17dc3a
Rogerio Almeida de Mattos <rogeriomattos38@outlook.com>
2023-09-18 11:28:42 -0300

    Adicionando form de novo usuario na home

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import UserItem from "../../components/UserItem";
 import { userListData } from "../../services/users/data";
 import "./styles.css";
+import UserForm from "../../components/UserForm";
+import { Dialog, trigerDialog } from "../../components/Dialog";
 
 const Home = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   const [userList, setUserList] = useState(userListData);
   const onDelete = (id: number) => {
     console.log({ id });
   };
 
+  const handleAdd = (data: any) => {
+    setUserList([
+      ...userList,
+      { ...data, id: userList[userList.length - 1].id + 1 },
+    ]);
+    trigerDialog();
+  };
+
   return (
     <div>
       <h1>HOME</h1>
+      <Dialog buttonText="Novo Usuário">
+        <h2>Novo Usuário</h2>
+        <UserForm onSave={handleAdd} />
+      </Dialog>
       <table>
-        {userList.map((item) => (
-          <UserItem key={item.id} onDelete={onDelete} {...item} />
-        ))}
+        <tbody>
+          {userList.map((item) => (
+            <UserItem key={item.id} onDelete={onDelete} {...item} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
